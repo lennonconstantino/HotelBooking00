b998f7ee620579d2da28fab2fc8f5260feace8e6
lennonconstantino <lennonconstantino@gmail.com>
2025-05-27 22:39:32 -0300

    My first commits

---
+++ Core/Domain/Entities/Guess.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Guess
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
